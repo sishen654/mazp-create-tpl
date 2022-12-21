7376f42add715f37da675886f2873d5405d7b85a
sishen <1730821420@qq.com>
2022-12-21 23:52:56 +0800

    update: delete cli default file

--- template/util-lib/rollup.config.js
+++ template/util-lib/rollup.config.js
@@ -121,4 +121,4 @@ function createConfig (configArr = [], format = ["cjs", "es"]) {
   return arr
 }
 
-export default createConfig(["src/index.ts", { input: "src/cli.ts", declaration: false, formatOnce: true }])
+export default createConfig(["src/index.ts"])
