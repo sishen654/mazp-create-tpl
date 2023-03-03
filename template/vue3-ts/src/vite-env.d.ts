6ddec2036b2ec2eb0c7048fa35167924334dbca1
sishen <1730821420@qq.com>
2023-03-03 14:39:25 +0800

    update: compatible .d.ts update

--- template/vue3-ts/src/vite-env.d.ts
+++ template/vue3-ts/src/vite-env.d.ts
@@ -1,11 +1,5 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-
 declare module "vue" {
   // this.xxx
   interface ComponentCustomProperties {
